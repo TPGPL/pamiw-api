ad442cb3208ca161aa4825f957eea6c1e7459188
TPGPL <TPGPL@users.noreply.github.com>
2023-10-24 23:31:57 +0200

    Initial commit

---
+++ src/test/java/pl/edu/pw/pamiwapi/PamiwApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.pamiwapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamiwApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
