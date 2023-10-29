2e09ab84d1b7f6c1da49fe99020612fe81497d55
TPGPL <TPGPL@users.noreply.github.com>
2023-10-29 21:58:21 +0100

    Finish author service

--- src/main/java/pl/edu/pw/pamiwapi/services/AuthorService.java
+++ src/main/java/pl/edu/pw/pamiwapi/services/AuthorService.java
@@ -4,6 +4,7 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.edu.pw.pamiwapi.dtos.AuthorDto;
 import pl.edu.pw.pamiwapi.models.Author;
 import pl.edu.pw.pamiwapi.repositories.AuthorRepository;
 import pl.edu.pw.pamiwapi.utils.ServiceResponse;
@@ -33,15 +34,7 @@ public class AuthorService {
         var violations = validator.validate(author);
 
         if (!violations.isEmpty()) {
-            var message = new StringBuilder();
-
-            for (var v : violations) {
-                message.append(v.getMessage()).append(" ");
-            }
-
-            return ServiceResponse.<Author>builder()
-                    .message(message.toString())
-                    .wasSuccessful(false).build();
+            return createInvalidResponse(violations);
         }
 
         return ServiceResponse.<Author>builder()
@@ -50,5 +43,58 @@ public class AuthorService {
                 .build();
     }
 
-    // TODO: UD
+    public ServiceResponse<Author> update(int id, Author author) {
+        var authorToUpdate = getById(id);
+
+        if (authorToUpdate == null) {
+            return ServiceResponse.<Author>builder()
+                    .message("The author with given ID was not found.")
+                    .build();
+        }
+
+        authorToUpdate.setName(author.getName());
+        authorToUpdate.setSurname(author.getSurname());
+        authorToUpdate.setEmail(author.getEmail());
+
+        var violations = validator.validate(authorToUpdate);
+
+        if (!violations.isEmpty()) {
+            return createInvalidResponse(violations);
+        }
+
+        return ServiceResponse.<Author>builder()
+                .data(repository.save(authorToUpdate))
+                .wasSuccessful(true)
+                .build();
+    }
+
+    public ServiceResponse<Author> delete(int id) {
+        repository.deleteById(id);
+
+        return ServiceResponse.<Author>builder()
+                .wasSuccessful(true)
+                .build();
+    }
+
+    public Author mapFromDto(AuthorDto dto) {
+        if (dto == null) return null;
+
+        return Author.builder()
+                .name(dto.getName())
+                .surname(dto.getSurname())
+                .email(dto.getEmail())
+                .build();
+    }
+
+    private ServiceResponse<Author> createInvalidResponse(Set<ConstraintViolation<Author>> violations) {
+        var message = new StringBuilder();
+
+        for (var v : violations) {
+            message.append(v.getMessage()).append(" ");
+        }
+
+        return ServiceResponse.<Author>builder()
+                .message(message.toString())
+                .build();
+    }
 }
