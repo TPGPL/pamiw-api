545d38738714ef293127e734c9faa669cc725ca3
TPGPL <TPGPL@users.noreply.github.com>
2023-10-29 22:59:57 +0100

    Add publisher service

---
+++ src/main/java/pl/edu/pw/pamiwapi/services/PublisherService.java
@@ -0,0 +1,81 @@
+package pl.edu.pw.pamiwapi.services;
+
+import jakarta.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.edu.pw.pamiwapi.dtos.PublisherDto;
+import pl.edu.pw.pamiwapi.models.Publisher;
+import pl.edu.pw.pamiwapi.repositories.PublisherRepository;
+import pl.edu.pw.pamiwapi.utils.ServiceResponse;
+
+@Service
+public class PublisherService {
+    private final PublisherRepository repository;
+    private final Validator validator;
+
+    @Autowired
+    public PublisherService(PublisherRepository repository, Validator validator) {
+        this.repository = repository;
+        this.validator = validator;
+    }
+
+    public Publisher getById(int id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public Iterable<Publisher> getAll() {
+        return repository.findAll();
+    }
+
+    public ServiceResponse<Publisher> create(Publisher publisher) {
+        var violations = validator.validate(publisher);
+
+        if (!violations.isEmpty()) {
+            return ServiceResponse.createInvalidResponse(violations);
+        }
+
+        return ServiceResponse.<Publisher>builder()
+                .wasSuccessful(true)
+                .data(repository.save(publisher))
+                .build();
+    }
+
+    public ServiceResponse<Publisher> update(int id, Publisher publisher) {
+        var publisherToUpdate = getById(id);
+
+        if (publisherToUpdate == null) {
+            return ServiceResponse.<Publisher>builder()
+                    .message("The author with given ID was not found.")
+                    .build();
+        }
+
+        publisherToUpdate.setName(publisher.getName());
+
+        var violations = validator.validate(publisherToUpdate);
+
+        if (!violations.isEmpty()) {
+            return ServiceResponse.createInvalidResponse(violations);
+        }
+
+        return ServiceResponse.<Publisher>builder()
+                .wasSuccessful(true)
+                .data(repository.save(publisherToUpdate))
+                .build();
+    }
+
+    public ServiceResponse<Publisher> delete(int id) {
+        repository.deleteById(id);
+
+        return ServiceResponse.<Publisher>builder()
+                .wasSuccessful(true)
+                .build();
+    }
+
+    public Publisher mapFromDto(PublisherDto dto) {
+        if (dto == null) return null;
+
+        return Publisher.builder()
+                .name(dto.getName())
+                .build();
+    }
+}
