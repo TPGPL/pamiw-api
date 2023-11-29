a04cdd81c40b6ca0693d9db568816d6daa993400
TPGPL <TPGPL@users.noreply.github.com>
2023-11-29 20:17:24 +0100

    Add user details service implementation

---
+++ src/main/java/pl/edu/pw/pamiwapi/services/AppUserDetailsService.java
@@ -0,0 +1,28 @@
+package pl.edu.pw.pamiwapi.services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.edu.pw.pamiwapi.repositories.UserRepository;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+    private final UserRepository repository;
+
+    public AppUserDetailsService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (!repository.existsByUsername(username)) {
+            throw new UsernameNotFoundException("User does not exist.");
+        }
+
+        var user = repository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User does not exist."));
+
+        return User.builder().username(user.getUsername()).password(user.getPassword()).build();
+    }
+}
