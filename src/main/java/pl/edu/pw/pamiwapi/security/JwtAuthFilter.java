b5efbb83961f9a2105d932fda6d83c7129508531
TPGPL <TPGPL@users.noreply.github.com>
2023-11-29 22:07:31 +0100

    Add jwt filter

---
+++ src/main/java/pl/edu/pw/pamiwapi/security/JwtAuthFilter.java
@@ -0,0 +1,44 @@
+package pl.edu.pw.pamiwapi.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import pl.edu.pw.pamiwapi.services.AppUserDetailsService;
+
+import java.io.IOException;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtGenerator generator;
+    @Autowired
+    private AppUserDetailsService service;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = getTokenFromRequest(request);
+
+        if (StringUtils.hasText(token) && generator.validateJwt(token)) {
+            var username = generator.getUsernameFromJwt(token);
+
+            var userDetails = service.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null);
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        var bearerToken = request.getHeader("Authorization");
+
+        return StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ") ? bearerToken.substring(7) : null;
+    }
+}
--- src/main/java/pl/edu/pw/pamiwapi/security/SecurityConfiguration.java
+++ src/main/java/pl/edu/pw/pamiwapi/security/SecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -35,6 +36,8 @@ public class SecurityConfiguration {
                         .anyRequest().authenticated())
                 .httpBasic((base) -> {});
 
+        http.addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
@@ -46,5 +49,10 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter() {
+        return new JwtAuthFilter();
     }
 }
