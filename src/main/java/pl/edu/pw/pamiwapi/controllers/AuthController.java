aad5271887fb0988e75799a85b94073b046c4fa7
TPGPL <TPGPL@users.noreply.github.com>
2023-12-25 19:37:12 +0100

    Refactor auth flow

--- src/main/java/pl/edu/pw/pamiwapi/controllers/AuthController.java
+++ src/main/java/pl/edu/pw/pamiwapi/controllers/AuthController.java
@@ -1,92 +1,52 @@
 package pl.edu.pw.pamiwapi.controllers;
 
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validator;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.pw.pamiwapi.model.dtos.UserLoginDto;
 import pl.edu.pw.pamiwapi.model.dtos.UserRegisterDto;
-import pl.edu.pw.pamiwapi.model.domain.UserEntity;
-import pl.edu.pw.pamiwapi.repositories.UserRepository;
-import pl.edu.pw.pamiwapi.services.JwtService;
-
-import java.util.Set;
+import pl.edu.pw.pamiwapi.services.UserService;
 
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin
 public class AuthController {
-    private final AuthenticationManager manager;
-    private final JwtService jwtService;
-    private final UserRepository repository;
-    private final PasswordEncoder encoder;
-    private final Validator validator;
+    private final UserService userService;
 
     @Autowired
-    public AuthController(AuthenticationManager manager, JwtService jwtService, UserRepository repository, PasswordEncoder encoder, Validator validator) {
-        this.manager = manager;
-        this.jwtService = jwtService;
-        this.repository = repository;
-        this.encoder = encoder;
-        this.validator = validator;
+    public AuthController(UserService userService) {
+
+        this.userService = userService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody UserRegisterDto dto) {
-        if (repository.existsByEmail(dto.getEmail()) || repository.existsByUsername(dto.getUsername())) {
-            return new ResponseEntity<>("User with this data already exists.", HttpStatus.UNPROCESSABLE_ENTITY);
+        var response = userService.createUser(dto);
+
+        if (!response.isWasSuccessful()) {
+            var message = response.getMessage() == null ? "Failed to register." : response.getMessage();
+
+            return new ResponseEntity<>(message, HttpStatus.UNPROCESSABLE_ENTITY);
         }
-
-        var violations = validator.validate(dto);
-
-        if (!violations.isEmpty()) {
-            return new ResponseEntity<>(prepareViolationMessage(violations), HttpStatus.UNPROCESSABLE_ENTITY);
-        }
-
-        UserEntity user = UserEntity.builder()
-                .username(dto.getUsername())
-                .email(dto.getEmail())
-                .password(encoder.encode(dto.getPassword()))
-                .build();
-
-        repository.save(user);
 
         return new ResponseEntity<>("User registered successfully.", HttpStatus.OK);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody UserLoginDto dto) {
-        if (!dto.getPassword().equals(dto.getConfirmPassword())) {
-            return new ResponseEntity<>("Passwords do not match.", HttpStatus.UNPROCESSABLE_ENTITY);
+    public ResponseEntity<String> login(@RequestBody UserLoginDto dto, HttpServletResponse servletResponse) {
+        var response = userService.authenticate(dto);
+
+        if (response.isWasSuccessful()) {
+            var cookie = new Cookie("jwtToken", response.getData());
+
+            cookie.setHttpOnly(true);
+            cookie.setMaxAge(30 * 60);
+            servletResponse.addCookie(cookie);
         }
 
-        var violations = validator.validate(dto);
-
-        if (!violations.isEmpty()) {
-            return new ResponseEntity<>(prepareViolationMessage(violations), HttpStatus.UNPROCESSABLE_ENTITY);
-        }
-
-        var auth = manager.authenticate(new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword()));
-
-        SecurityContextHolder.getContext().setAuthentication(auth);
-
-        var token = jwtService.generateJwt(auth.getName());
-
-        return new ResponseEntity<>(token, HttpStatus.OK);
-    }
-
-    private <T> String prepareViolationMessage(Set<ConstraintViolation<T>> violations) {
-        var err = new StringBuilder();
-
-        for (var v : violations)
-            err.append(v.getMessage()).append(" ");
-
-        return err.toString().strip();
+        return new ResponseEntity<>(response.getMessage(), response.isWasSuccessful() ? HttpStatus.OK : HttpStatus.FORBIDDEN);
     }
 }
--- src/main/java/pl/edu/pw/pamiwapi/services/UserService.java
+++ src/main/java/pl/edu/pw/pamiwapi/services/UserService.java
@@ -1,22 +1,35 @@
 package pl.edu.pw.pamiwapi.services;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.edu.pw.pamiwapi.model.ServiceResponse;
 import pl.edu.pw.pamiwapi.model.domain.UserEntity;
+import pl.edu.pw.pamiwapi.model.dtos.UserLoginDto;
+import pl.edu.pw.pamiwapi.model.dtos.UserRegisterDto;
 import pl.edu.pw.pamiwapi.repositories.UserRepository;
+
+import java.util.Set;
 
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository repository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final Validator validator;
 
     @Autowired
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, JwtService jwtService, PasswordEncoder passwordEncoder, Validator validator) {
         this.repository = repository;
+        this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
+        this.validator = validator;
     }
 
     public ServiceResponse<UserEntity> getUserByUsername(String username) {
@@ -34,5 +47,56 @@ public class UserService implements UserDetailsService {
         }
 
         return User.builder().username(user.getUsername()).password(user.getPassword()).build();
+    }
+
+    public ServiceResponse<UserEntity> createUser(UserRegisterDto dto) {
+        if (repository.existsByEmail(dto.getEmail()) || repository.existsByUsername(dto.getUsername())) {
+            return ServiceResponse.<UserEntity>builder().wasSuccessful(false).message("User with this data already exists.").build();
+        }
+
+        var violations = validator.validate(dto);
+
+        if (!violations.isEmpty()) {
+            return ServiceResponse.<UserEntity>builder().wasSuccessful(false).message(prepareViolationMessage(violations)).build();
+        }
+
+        var user = UserEntity.builder()
+                .username(dto.getUsername())
+                .email(dto.getEmail())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .build();
+
+        return ServiceResponse.<UserEntity>builder().wasSuccessful(true).data(repository.save(user)).build();
+    }
+
+    public ServiceResponse<String> authenticate(UserLoginDto dto) {
+        if (!dto.getPassword().equals(dto.getConfirmPassword())) {
+            return ServiceResponse.<String>builder().wasSuccessful(false).message("Password do not match.").build();
+        }
+
+        var violations = validator.validate(dto);
+
+        if (!violations.isEmpty()) {
+            return ServiceResponse.<String>builder().wasSuccessful(false).message(prepareViolationMessage(violations)).build();
+        }
+
+        var user = getUserByUsername(dto.getUsername()).getData();
+
+        if (user == null || !passwordEncoder.matches(dto.getPassword(), user.getPassword())) {
+            return ServiceResponse.<String>builder().wasSuccessful(false).message("Failed to login.").build();
+        }
+
+        var token = jwtService.generateJwt(user.getUsername());
+
+        return ServiceResponse.<String>builder().wasSuccessful(true).message("Authenticated.").data(token).build();
+    }
+
+    private <T> String prepareViolationMessage(Set<ConstraintViolation<T>> violations) {
+        var err = new StringBuilder();
+
+        for (var v : violations)
+            err.append(v.getMessage()).append(" ");
+
+        return err.toString().strip();
     }
 }
